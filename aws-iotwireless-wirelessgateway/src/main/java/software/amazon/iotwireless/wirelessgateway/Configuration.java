8284d930a3a87439d6072ac2bebb3a46a19d41f2
Moira Huang <moirah@amazon.com>
2021-02-12 12:05:47 -0800

    jsons

--- aws-iotwireless-wirelessgateway/src/main/java/software/amazon/iotwireless/wirelessgateway/Configuration.java
+++
@@ -1,8 +0,0 @@
-package software.amazon.iotwireless.wirelessgateway;
-
-class Configuration extends BaseConfiguration {
-
-    public Configuration() {
-        super("aws-iotwireless-wirelessgateway.json");
-    }
-}
