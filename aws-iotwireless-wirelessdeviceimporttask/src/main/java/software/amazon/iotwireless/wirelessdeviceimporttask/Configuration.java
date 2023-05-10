e7529ff3c6bcaa5ebcba08cd1657a1481b07d9c4
Aarati Sonawane <aaratis@amazon.com>
2023-05-10 16:52:39 -0700

    clean up

--- aws-iotwireless-wirelessdeviceimporttask/src/main/java/software/amazon/iotwireless/wirelessdeviceimporttask/Configuration.java
+++
@@ -1,8 +0,0 @@
-package software.amazon.iotwireless.wirelessdeviceimporttask;
-
-class Configuration extends BaseConfiguration {
-
-    public Configuration() {
-        super("aws-iotwireless-wirelessdeviceimporttask.json");
-    }
-}
